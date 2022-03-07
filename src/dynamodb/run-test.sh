404e7b286bdc28aca697500c3e48fd4f163e9c4a
ll <liuguihan@gmail.com>
2022-03-06 21:28:36 -0500

    Update unit test cases

--- src/dynamodb/run-test.sh
+++ src/dynamodb/run-test.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
 echo "List all the tables of Dynamodb"
-
 aws dynamodb list-tables
 
-echo "Let us choose table my-table-better-troll"
-table_name='my-table-better-troll'
+echo "Let us choose a new table for testing"
+table_name='new-table-for-test'
 
 echo "Put items into the table"
 aws dynamodb put-item --table-name $table_name --item '{"Artist": {"S": "No One You Know"}, "SongTitle": {"S": "Call Me Today"}, "AlbumTitle": {"S": "Somewhat Famous"}}' --return-consumed-capacity TOTAL
