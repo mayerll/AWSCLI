1bf90968f21e0252b2315f98d806ecb32edbb3e3
ll <liuguihan@gmail.com>
2022-03-06 21:51:43 -0500

    Update Dockerfile

--- src/dynamodb/run-test.sh
+++ src/dynamodb/run-test.sh
@@ -5,6 +5,17 @@ aws dynamodb list-tables
 
 echo "Let us choose a new table for testing"
 table_name='new-table-for-test'
+aws dynamodb create-table \
+    --table-name $table_name \
+    --attribute-definitions \
+        AttributeName=Artist,AttributeType=S \
+        AttributeName=SongTitle,AttributeType=S \
+    --key-schema \
+        AttributeName=Artist,KeyType=HASH \
+        AttributeName=SongTitle,KeyType=RANGE \
+    --provisioned-throughput \
+        ReadCapacityUnits=10,WriteCapacityUnits=5 \
+    --table-class STANDARD
 
 echo "Put items into the table"
 aws dynamodb put-item --table-name $table_name --item '{"Artist": {"S": "No One You Know"}, "SongTitle": {"S": "Call Me Today"}, "AlbumTitle": {"S": "Somewhat Famous"}}' --return-consumed-capacity TOTAL
